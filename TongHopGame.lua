c063422694ba98bbcaf9058b580924ecc7917ba3
Cathub-Script <boyngoc8386@gmail.com>
2025-06-07 15:21:10 +0700

    Add files via upload

---
+++ TongHopGame.lua
@@ -0,0 +1,14 @@
+local placeId = game.PlaceId
+if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
+	--Blox Fruits
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/binthoiskidibi/TongHopScript/refs/heads/main/ScriptTonghop.lua"))()
+elseif placeId == 205224386 then
+	--HideAndSeek
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-BlueX/BlueX-Hub/refs/heads/main/HideAndSeek.lua"))()
+elseif placeId == 116495829188952 or placeId == 70876832253163 then
+    	--DeadRails
+    	loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-BlueX/BlueX-Hub/main/DeadRails.lua"))()
+elseif placeId == 126884695634066 then
+    	--GrowaGarden
+    	loadstring(game:HttpGet("https://raw.githubusercontent.com/Cathub-Script/ALLMap/refs/heads/main/Grow%20a%20Garden.lua"))()
+end
